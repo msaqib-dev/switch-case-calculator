1dc2e51544c913821730fc69a321d2e6262f6bb8
Muhammad Saqib <77567399+msaqib-dev@users.noreply.github.com>
2024-03-28 16:31:50 +0500

    Push Simple Calculator to Github

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        name: "firstNumber",
+        type: "number",
+        message: "Enter your first number",
+    },
+    {
+        name: "secondNumber",
+        type: "number",
+        message: "Enter your second number",
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Select the operator to perform action",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division",
+            "Exponent",
+        ],
+    },
+]);
+switch (answer.operator) {
+    case (answer.operator = "Addition"):
+        console.log(answer.firstNumber + answer.secondNumber);
+        break;
+    case (answer.operator = "Subtraction"):
+        console.log(answer.firstNumber - answer.secondNumber);
+        break;
+    case (answer.operator = "Multiplication"):
+        console.log(answer.firstNumber * answer.secondNumber);
+        break;
+    case (answer.operator = "Division"):
+        console.log(answer.firstNumber / answer.secondNumber);
+        break;
+    case (answer.operator = "Exponent"):
+        console.log(answer.firstNumber ** answer.secondNumber);
+        break;
+}
